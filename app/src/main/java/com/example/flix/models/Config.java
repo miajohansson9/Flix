176286f7f3efae1f1b280acde9035eeb075509df
Mia Johansson <miajohansson@fb.com>
2019-06-28 11:30:40 -0700

    Initial commit

---
+++ app/src/main/java/com/example/flix/models/Config.java
@@ -0,0 +1,37 @@
+package com.example.flix.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Config {
+    String imageBaseUrl;
+    String posterSize;
+    String backdropSize;
+    String backdropPath;
+
+    public Config(JSONObject object) throws JSONException {
+        JSONObject images = object.getJSONObject("images");
+        imageBaseUrl = images.getString("secure_base_url");
+        JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
+        posterSize = posterSizeOptions.optString(3, "w342");
+        JSONArray backdropSizeOptions = images.getJSONArray("backdrop_sizes");
+        backdropSize = backdropSizeOptions.optString(1, "w780");
+    }
+
+    public String getImageUrl(String size, String path) {
+        return String.format("%s%s%s", imageBaseUrl, size, path);
+    }
+
+    public String getImageBaseUrl() {
+        return imageBaseUrl;
+    }
+
+    public String getPosterSize() {
+        return posterSize;
+    }
+
+    public String getBackdropSize() {
+        return backdropSize;
+    }
+}
---
+++ app/src/main/java/com/example/flix/movieAdapter.java
@@ -0,0 +1,99 @@
+package com.example.flix;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.flix.models.Config;
+import com.example.flix.models.Movie;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
+public class movieAdapter extends RecyclerView.Adapter<movieAdapter.ViewHolder>{
+    ArrayList<Movie> movies;
+    Config config;
+    Context context;
+
+    public movieAdapter(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public Config getConfig() {
+        return config;
+    }
+
+    public void setConfig(Config config) {
+        this.config = config;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        context = viewGroup.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View movieView = inflater.inflate(R.layout.item_movie, viewGroup, false);
+
+        return new ViewHolder(movieView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Movie movie = movies.get(i);
+        viewHolder.tvTitle.setText(movie.getTitle());
+        viewHolder.tvOverview.setText(movie.getOverview());
+
+        boolean isPortrait = context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+
+        String imageURL;
+        if (isPortrait) {
+            imageURL = config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        } else {
+            imageURL = config.getImageUrl(config.getBackdropSize(), movie.getBackdropPath());
+        }
+
+        int placeholderId = isPortrait ? R.drawable.flicks_movie_placeholder : R.drawable.flicks_backdrop_placeholder;
+        ImageView imageView = isPortrait ? viewHolder.tvPosterImage : viewHolder.tvBackdropImage;
+
+        Glide.with(context)
+                .load(imageURL)
+                .bitmapTransform(new RoundedCornersTransformation(context, 25, 0))
+                .placeholder(R.drawable.flicks_movie_placeholder)
+                .error(placeholderId)
+                .into(imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView tvPosterImage;
+        ImageView tvBackdropImage;
+        TextView tvTitle;
+        TextView tvOverview;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvPosterImage = (ImageView) itemView.findViewById(R.id.tvPosterImage);
+            tvBackdropImage = (ImageView) itemView.findViewById(R.id.tvBackdropImage);
+            tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
+            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+        }
+    }
+}
